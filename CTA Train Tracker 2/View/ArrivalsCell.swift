0a739123a9f219358252b8d3556a07e5769eeb39
Tommy Bart <tommybart8@gmail.com>
2019-08-09 13:59:31 -0500

    Created station & route labels

--- CTA Train Tracker 2/View/ArrivalsCell.swift
+++ CTA Train Tracker 2/View/ArrivalsCell.swift
@@ -12,22 +12,38 @@ class ArrivalsCell: BaseCell {
     
     let trainImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.green
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.green
         return imageView
     }()
     
     let arrivalsTextView: UITextView = {
         let textView = UITextView()
-        textView.backgroundColor = UIColor.gray
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = UIColor.gray
         return textView
+    }()
+    
+    let stationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.orange
+        return label
+    }()
+    
+    let routeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.blue
+        return label
     }()
     
     override func setupViews() {
         super.setupViews()
         addSubview(trainImageView)
         addSubview(arrivalsTextView)
+        addSubview(stationLabel)
+        addSubview(routeLabel)
         
         // Horizontal Constraints
         addConstraintsWithFormat(format: "H:|-16-[v0(44)]", views: trainImageView)
@@ -35,6 +51,18 @@ class ArrivalsCell: BaseCell {
         
         // Vertical Constraints
         addConstraintsWithFormat(format: "V:|-16-[v0(44)]-8-[v1]-16-|", views: trainImageView, arrivalsTextView)
+        
+        // Station Label Constraints
+        addConstraint(NSLayoutConstraint(item: stationLabel, attribute: .bottom, relatedBy: .equal, toItem: routeLabel, attribute: .top, multiplier: 1, constant: -2))
+        addConstraint(NSLayoutConstraint(item: stationLabel, attribute: .left, relatedBy: .equal, toItem: trainImageView, attribute: .right, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: stationLabel, attribute: .right, relatedBy: .equal, toItem: arrivalsTextView, attribute: .right, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: stationLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 22))
+        
+        // Route Label Constraints
+        addConstraint(NSLayoutConstraint(item: routeLabel, attribute: .bottom, relatedBy: .equal, toItem: arrivalsTextView, attribute: .top, multiplier: 1, constant: -6))
+        addConstraint(NSLayoutConstraint(item: routeLabel, attribute: .left, relatedBy: .equal, toItem: trainImageView, attribute: .right, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: routeLabel, attribute: .right, relatedBy: .equal, toItem: arrivalsTextView, attribute: .right, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: routeLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 22))
     }
 }
 
